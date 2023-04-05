2867005d33b01477c3761705c014bbd5550a1fb0
ttm-mk <129599412+ttm-mk@users.noreply.github.com>
2023-04-05 15:22:14 +0000

    [Update]成功フラッシュ機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,8 +2,12 @@ class BooksController < ApplicationController
   
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    if book.save
+      flash[:notice] = "The book was submitted successfully."
+      redirect_to book_path(book.id)
+    else
+      render :index
+    end
   end
   
   def index
@@ -22,6 +26,7 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
+    flash[:notice] = "The book was successfully updated."
     redirect_to book_path(book.id)
   end
   
