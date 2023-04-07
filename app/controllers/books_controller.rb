3cca8a116798f6a98b6d14e712e215dc7da0b8c6
ttm-mk <129599412+ttm-mk@users.noreply.github.com>
2023-04-07 13:48:06 +0000

    [Update]更新バリデーション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,10 @@
 class BooksController < ApplicationController
   
   def create
-    book = Book.new(book_params)
-    if book.save
+    @book = Book.new(book_params)
+    if @book.save
       flash[:notice] = "The book was submitted successfully."
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
       render :index
     end
@@ -24,10 +24,13 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    flash[:notice] = "The book was successfully updated."
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "The book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
   
   def destroy
